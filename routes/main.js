bbd47ef3c4f403b38bc7d337ce5e638c3e623a71
Aditya <87621695+muzhx@users.noreply.github.com>
2021-07-20 08:33:50 +0900

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -11,15 +11,23 @@ router.get('/api', (req, res) => {
     res.sendFile(__path + '/views/index.html')
 })
 
+/*router.get('/api/about', (req, res) => {
+    res.sendFile(__path + '/views/about.html')
+})*/
+
+router.get('/api/news', (req, res) => {
+    res.redirect('https://raw.githubusercontent.com/Zhirrr/My-SQL-Results/main/Berita.json')
+})
+
 router.get('/config', (req, res) => {
     config = {
         status: true,
         result: {
-            prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
-            youtube : 'Gak Punya'
+            prefix : '.',
+            namabot: 'muzhabot',
+            namaowner: 'muzha',
+            instagram: '-',
+            youtube : 'muzha'
         }
     }
     res.json(config)
