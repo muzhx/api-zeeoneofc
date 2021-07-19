b0feab3399b18efe87fef77bd68606ea66953a93
Aditya <87621695+muzhx@users.noreply.github.com>
2021-07-20 08:46:45 +0900

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -23,11 +23,11 @@ router.get('/config', (req, res) => {
     config = {
         status: true,
         result: {
-            prefix : 'z',
-            namabot: 'Alphabot',
-            namaowner: 'zeeone',
-            instagram: 'zee.oneee',
-            youtube : 'wandi'
+            prefix : '.',
+            namabot: 'muzhabot',
+            namaowner: 'muzha',
+            instagram: '-',
+            youtube : 'muzha'
         }
     }
     res.json(config)
